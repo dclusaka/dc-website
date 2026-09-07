ad7a554c3aa6985f639b452c945985b15c487669
dclusaka <dclusaka@gmail.com>
2026-09-07 22:14:38 +0900

    Add files via upload

---
+++ src/eleventy.config.js
@@ -0,0 +1,21 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/_redirects");
+
+  eleventyConfig.addCollection("events", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/events/*.md").sort((a, b) => {
+      return (a.data.order || 0) - (b.data.order || 0);
+    });
+  });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+  };
+};
